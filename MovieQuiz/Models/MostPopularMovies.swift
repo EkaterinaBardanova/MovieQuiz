5650dc65d7a2855effb82573daf868da0f60e5ab
Ekaterina <bardanovaekaterina7@gmail.com>
2026-01-08 16:13:53 +0100

    Add data loading and error handling logic for sprint 06 task

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Екатерина Барданова on 6. 1. 2026..
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Екатерина Барданова on 6. 1. 2026..
+//
+
+import Foundation
